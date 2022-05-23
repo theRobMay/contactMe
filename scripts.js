a9fe9a4e265741ed5b87502bb1051c70ce8b6f00
theRobMay <robbie.maysent@gmail.com>
2022-05-22 22:32:47 -0500

    "i think i did something but im not sure what"

--- scripts.js
+++ scripts.js
@@ -2,5 +2,5 @@ const placeForResults = document.getElementById("results")
 
 new URLSearchParams(window.location.search).forEach((value, name) => {
     placeForResults.append(`${name} : ${value}`)
-    placeForResults.append(document.createElement("br"))
+    placeForResults.append(document.createElement("resultForm"))
 })
